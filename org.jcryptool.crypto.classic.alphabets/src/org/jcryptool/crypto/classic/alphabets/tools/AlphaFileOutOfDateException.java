300a00405993032182e0bc7417e258be18a88e75
anatolibarski <nomail@mail.com>
2013-08-18 18:55:44 +0200

    removed e3 bundles from e4 branch

--- org.jcryptool.crypto.classic.alphabets/src/org/jcryptool/crypto/classic/alphabets/tools/AlphaFileOutOfDateException.java
+++
@@ -1,9 +0,0 @@
-package org.jcryptool.crypto.classic.alphabets.tools;
-
-public class AlphaFileOutOfDateException extends Exception {
-
-	public AlphaFileOutOfDateException(String string) {
-		super(string);
-	}
-
-}
--- org.jcryptool.crypto.classic.substitution/src/org/jcryptool/crypto/classic/substitution/algorithm/SubstitutionKeyValidityException.java
+++
@@ -1,9 +0,0 @@
-package org.jcryptool.crypto.classic.substitution.algorithm;
-
-public class SubstitutionKeyValidityException extends Exception {
-
-	public SubstitutionKeyValidityException(String message) {
-		super(message);
-	}
-
-}
